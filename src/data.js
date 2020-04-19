84caa78d2561b49f9e16f3a727c845c34e719dd2
David B <david_beale@outlook.com>
2020-04-19 16:38:23 +0100

    feat: auto add row if times conflict

--- src/data.js
+++ src/data.js
@@ -1,11 +1,51 @@
 export default 
 [
   {
-    title: 'Show',
-    description: 'Cool show',
+    title: 'Show 1',
+    description: 'Cool show description',
     image: 'TODO',
     date: new Date('2020-04-22'),
     time: [16, 45],
     length: [3, 15]
+  },
+  {
+    title: 'Show 2',
+    description: 'Cool show description',
+    image: 'TODO',
+    date: new Date('2020-04-22'),
+    time: [15, 45],
+    length: [1, 0]
+  },
+  {
+    title: 'Show 3',
+    description: 'Cool show description',
+    image: 'TODO',
+    date: new Date('2020-04-22'),
+    time: [15, 45],
+    length: [1, 0]
+  },
+  {
+    title: 'Show 4',
+    description: 'Cool show description',
+    image: 'TODO',
+    date: new Date('2020-04-22'),
+    time: [15, 45],
+    length: [1, 0]
+  },
+  {
+    title: 'Show 5',
+    description: 'Cool show description',
+    image: 'TODO',
+    date: new Date('2020-04-22'),
+    time: [15, 45],
+    length: [1, 0]
+  },
+  {
+    title: 'Show 6',
+    description: 'Cool show description',
+    image: 'TODO',
+    date: new Date('2020-04-22'),
+    time: [12, 45],
+    length: [1, 0]
   },
 ]
