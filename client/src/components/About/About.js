50ab1c0a5edfe7ab7c9858e7952d3719888c8529
Pawel <pawel.pietruszka99@gmail.com>
2020-04-26 12:46:51 +0200

    added calendar config

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import './About.css';
 
 export default function About() {
+  
+
   return (
     <div className="about-container">
       <h1 style={{ color: 'orange' }}>About us</h1>
