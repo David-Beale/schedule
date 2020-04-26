3363995fe35f529440fab2243567adbe4ecaaab7
giovannistasi <gsstasi@gmail.com>
2020-04-26 16:39:22 +0200

    fix: userEventsList now updating on delete

--- client/src/components/UserEventsList/UserEventsList.js
+++ client/src/components/UserEventsList/UserEventsList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { getCurrentUser } from '../../stitch/authentication';
 import { useDispatch } from 'react-redux';
 import { loadEventsByUserId, removeEvent } from '../../redux/actions';
@@ -8,38 +8,27 @@ import OwnCard from '../OwnCard/OwnCard';
 function UserEventsList() {
   const dispatch = useDispatch();
   const data = useSelector(({ eventsReducer }) => eventsReducer.myEvents);
-  const [myEvents, setMyEvents] = useState([])
+  const user = getCurrentUser().id
 
   useEffect(() => {
-    let user = getCurrentUser().id
     dispatch(loadEventsByUserId(user))
   }, []);
 
-  useEffect(() => {
-    if (data) {
-      let array = data.map(event => event)
-      setMyEvents(array)
-    }
-  }, [data])
-
   const handleDelete = (id) => {
     dispatch(removeEvent(id));
+    dispatch(loadEventsByUserId(user))
   }
 
   return (
     <div className='event-container'>
-      {myEvents.length > 0 && myEvents.sort((a,b) => b.date - a.date)
-        .map((info, index) => {
-        return <OwnCard info={info.eventData} id={info._id} key={index} handleDelete={handleDelete} />
-      })}
-      {/* {data.map((info, index) => {
+      {data.map((info, index) => {
         return <OwnCard
           info={info.eventData}
           key={index}
           id={info._id}
           handleDelete={handleDelete}
         />
-      })} */}
+      })}
     </div>
   )
 }
